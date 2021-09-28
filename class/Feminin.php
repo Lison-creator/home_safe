d79fc5d8317aa2088d9620f4cf790a19a7d773d0
Maureen Vanhoren <maureenvanhoren@gmail.com>
2021-09-28 14:10:22 +0200

    travail sur la classe Feminin

---
+++ class/Feminin.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once("Utilisateur.php");
+    class Feminin extends Utilisateur {
+        // va récupérer les propriétés de l'Utilisateur mais on va préciser des caractéristiques
+    }
+?>
