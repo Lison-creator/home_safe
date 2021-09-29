27cd2790be983912ad89982436d40d610989d821
Manon Lespes <manonlespes@hotmail.fr>
2021-09-29 09:49:27 +0200

    addition des class objets

---
+++ class/Lieu.php
@@ -0,0 +1,17 @@
+
+<?php 
+
+class Utilisateur {
+        // PROPRIETES
+        // La classe qui englobe toutes les autres classes au niveau du genre
+        protected $type;
+        protected $localite; // relier à MySQL de type composite (rue, numéro, boite...) ???
+        protected $bio;
+
+        // CONSTRUCTEUR
+        public function __construct() 
+        {
+            
+        }
+
+?>
