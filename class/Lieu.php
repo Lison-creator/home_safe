915e7e354c81d643cd5a184d9fe86ab516995e54
Manon Lespes <manonlespes@hotmail.fr>
2021-09-29 12:24:02 +0200

    travaille sur une class

--- class/Lieu.php
+++ class/Lieu.php
@@ -1,18 +1,44 @@
-
 <?php 
 
-class Utilisateur {
+class Lieu {
         // PROPRIETES
-        // La classe qui englobe toutes les autres classes au niveau du genre
         protected $type;
-        protected $localite; // relier à MySQL de type composite (rue, numéro, boite...) ???
-        protected $bio;//private : ne peut être modifié qu'à l'intérieur de la classe
+        protected $adresse; // relier à MySQL de type composite (rue, numéro, boite...) ???
+        //protected $num;
+        //protected $boite;
+        //protected $rue;
+        //protected $zip;
+        //protected $localite;
+        protected $description;//private : ne peut être modifié qu'à l'intérieur de la classe
 
         
         // CONSTRUCTEUR
-        public function __construct() 
+        public function __construct($_type, $_adresse, $_description) 
         {
-            
+            $this->type = $type;
+            $this->adresse = $adresse;
+            $this->description = $description;
         }
+
+        // MÉTHODES
+        public lieuAjoute (){
+            return '<p>'. $this->type . ' ' . $this->adresse . ' ' . $this->description . '</p>';
+        }
+
+        /* public lieuModifie(){
+
+        }
+
+        public lieuPartage(){
+
+        } 
+        
+        public adresseEntiere(){
+            return $this->rue . ' ' . $this->num . ' , boîte ' . $this->boite . '<br />' . $this->zip . ' ' . $this->localite; 
+        }
+    
+        
+        
+        */
 
 ?>
