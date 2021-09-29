da95401de784acb5c686c2a85ef6707c037ae36d
Lison <lison.ferne@wanadoo.fr>
2021-09-29 14:59:51 +0200

    Boutons !!

--- view/page/accueil.php
+++ view/page/accueil.php
@@ -2,9 +2,13 @@
     <h2>Accueil</h2>
     <h3>Accueil</h3>
     <h4>Accueil</h4>
-   <!-- Image de la carte à la place --> <img src="public/images/cacahuete-front.jpg" alt="Image cacahuete">
-   <!-- Trois boutons: "partager", "chercher un trajet", "rejoindre une communauté" -->
-   <!-- + menu déroulant en bas à gauche -->
+    <!-- Image de la carte à la place --> <img src="public/images/cacahuete-front.jpg" alt="Image cacahuete">
+    <!-- Trois boutons: "partager", "chercher un trajet", "rejoindre une communauté" -->
+    <!-- + menu déroulant en bas à gauche -->
+
+    <button class="btnJaune">Coucou</button>
+    <button class="btnViolet">Violet</button>
+    <button class="btnBleu">Bleu</button>
 </div>
 <p>
 
