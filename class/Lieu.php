57f1620852c67a2ad1d038e60658782f6d65d55f
Manon Lespes <manonlespes@hotmail.fr>
2021-09-29 09:55:11 +0200

    test

--- class/Lieu.php
+++ class/Lieu.php
@@ -6,8 +6,9 @@ class Utilisateur {
         // La classe qui englobe toutes les autres classes au niveau du genre
         protected $type;
         protected $localite; // relier à MySQL de type composite (rue, numéro, boite...) ???
-        protected $bio;
+        protected $bio;//private : ne peut être modifié qu'à l'intérieur de la classe
 
+        
         // CONSTRUCTEUR
         public function __construct() 
         {
