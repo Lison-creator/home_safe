c5b0d636ee333ada3e459e0d200187aa07c8ed73
Lison <lison.ferne@wanadoo.fr>
2021-09-29 13:25:55 +0200

    Quartier Lison 29-09-21 13:25

--- class/Quartier.php
+++ class/Quartier.php
@@ -1,5 +1,65 @@
 <?php 
     class Quartier {
-        
+
+        /* Propriétés */
+
+        protected $nom;
+        protected $codePostal;
+        protected $localisation; /* coordonnées géographiques */
+        protected $quotation; /* Une note donnée par les utilisateur.ice.s ? */
+        protected $description; /* Texte de description du quartier */
+
+        /* CONSTRUCTEUR */
+
+        public function __construct($_nom, $_codePostal, $_localisation, $_quotation, $_description){
+            $this->nom = $_nom;
+            $this->codePostal = $_codePostal;
+            $this->localisation = $_localisation;
+            $this->quotation = $_quotation;
+            $this->description = $_description;
+           
+        }
+
+        /* Setter - Getter */
+
+        public function setNom($_nom){
+            $this->nom = $_nom;
+        }
+        public function getNom(){
+            return $this->nom;
+        }
+
+        public function setCodePostal($_codePostal){
+            $this->codePostal = $_codePostal;
+        }
+        public function getCodePostal(){
+            return $this->codePostal;
+        }
+
+        public function setLocalisation($_localisation){
+            $this->localisation = $_localisation;
+        }
+        public function getLocalisation(){
+            return $this->localisation;
+        } 
+
+        public function setQuotation($_quotation){
+            $this->quotation = $_quotation;
+        }
+        public function getQuotation(){
+            return $this->quotation;
+        }
+
+        public function setDescription($_description){
+            $this->description = $_description;
+        }
+        public function getDescription(){
+            return $this->description;
+        }
+
+
+
+
+
     }
 ?>
