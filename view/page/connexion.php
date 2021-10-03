fe3082ebbb5714639a56b17d618dc41ebba2ae27
Manon Lespes <manonlespes@hotmail.fr>
2021-10-03 17:16:13 +0200

    travaille sur la page connection avec feuille de style

--- view/page/connexion.php
+++ view/page/connexion.php
@@ -1,10 +1,36 @@
-<div class="container">
-    <!-- Un formulaire html complet pour la connexion -->
-    <h2>Connexion</h2>
-    <form action="#" method="post">
-        <label for="nom">Nom :</label>
-        <input type="text" name="nom" id="nom">
-        <input type="submit" value="Connexion">
-    </form>
-    <?= $msgError; ?> 
-</div>
+<!-- <link rel="stylesheet" href="styleConnection.css"> -->
+<!-- Faut-il le relier ici aussi où uniquement dans le head.php ? -->
+
+<section class="container-flex">
+    <!-- Un formulaire html complet pour la connection -->
+    <div class="row">
+        <div class="col-5 offset-1 formulaireConnection">
+            <h2 class="text-uppercase mb-2 mt-5 text-center">Home Safe</h2>
+            <h3 class="text-center">Rejoins Home Safe et ta communauté pour des trajets sécures dans Bruxelles</h3>
+
+            <div class="container-flex fondFormulaire">
+                <div class="row">
+                    <a href="" class="col text-center mt-3">Se connecter</a>
+                    <a href="" class="col text-center mt-3">S'inscrire</a>
+                </div>
+                <form action="#" method="post" class="text-center">
+
+                    <label for="nom" id="labelConnectionPrenom">Prénom</label><br> <!-- TODO modifier le name et le for dans php et le routeur pour prénom -->
+                    <input type="text" name="nom" id="nom"><br>
+
+                    <label for="emailConnection" id="labelConnectionEmail">E-mail</label><br>
+                    <input type="mail" name="emailConnection" id="emailConnection"><br>
+
+                    <label for="motDePasseConnection" id="labelConnectionMdp">Mot de passe</label><br>
+                    <input type="password" name="motDePasseConnection" id="motDePasseConnection"><br>
+
+                    <div class="text-center">
+                        <input type="submit" value="Se connecter" class="btnJaune btnConnection">
+                    </div>
+                </form>
+            </div>
+            <?= $msgError; ?>
+        </div>
+
+    </div>
+</section>
