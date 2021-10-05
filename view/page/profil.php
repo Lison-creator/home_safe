c11f84b4e22581a71db2ff9d8b78b323897f1874
Lison <lison.ferne@wanadoo.fr>
2021-10-05 17:12:59 +0200

    Squelette de la page Profil

--- view/page/profil.php
+++ view/page/profil.php
@@ -6,23 +6,42 @@
 <!-- Petit menu vertical: "Nouveautés", "Ma communauté", "Lieux favoris" -->
 <!-- La carte interactive -->
 <!-- Le logo -->
+<link rel="stylesheet" href="profil.css">
 
 <div class="container">
-    <nav>
+    <nav class="d-flex flex-column">
+        <!-- Petit menu qui tombe en haut à droite -->
 
+        <div>Nouveautés</div>
+        <div>Ma communauté</div>
+        <div>Favoris</div>
     </nav>
     <div>
-        <!-- logo -->
+        <!-- logo en haut à gauche -->
         <img src="" alt="logo">
     </div>
-    <aside>
+    <div>
+        <!-- input "rechercher un lieu" -->
+        <input type="text">
+        <!-- Grand "plus" -->
+        <svg id="_Color" data-name=" ↳Color" xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56">
+            <path id="_Color-2" data-name=" ↳Color" d="M56,32H32V56H24V32H0V24H24V0h8V24H56Z" fill="#fff" />
+        </svg>
 
-        <div>
-            <svg id="_Color" data-name=" ↳Color" xmlns="http://www.w3.org/2000/svg" width="25.04" height="15.442" viewBox="0 0 25.04 15.442">
-                <path id="_Color-2" data-name=" ↳Color" d="M2.938,0,0,2.942,9.544,12.52,0,22.1,2.938,25.04l12.5-12.52Z" transform="translate(25.04) rotate(90)" fill="#707070" />
-            </svg>
+    </div>
+    <div class="row">
+        <aside class="col-4">
 
-        </div>
-
+            <div>
+                <!-- Petite flêche vers le bas pour les options de compte -->
+                <svg id="_Color" data-name=" ↳Color" xmlns="http://www.w3.org/2000/svg" width="25.04" height="15.442" viewBox="0 0 25.04 15.442">
+                    <path id="_Color-2" data-name=" ↳Color" d="M2.938,0,0,2.942,9.544,12.52,0,22.1,2.938,25.04l12.5-12.52Z" transform="translate(25.04) rotate(90)" fill="#707070" />
+                </svg>
+                <!-- Icone "commentaires récents" -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                    <path id="Icon_feather-message-circle" data-name="Icon feather-message-circle" d="M31.5,17.25a12.57,12.57,0,0,1-1.35,5.7A12.75,12.75,0,0,1,18.75,30a12.57,12.57,0,0,1-5.7-1.35L4.5,31.5l2.85-8.55A12.57,12.57,0,0,1,6,17.25a12.75,12.75,0,0,1,7.05-11.4,12.57,12.57,0,0,1,5.7-1.35h.75a12.72,12.72,0,0,1,12,12Z" transform="translate(-3 -3)" fill="none" stroke="#707070" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
+                </svg>
+            </div>
+    </div>
     </aside>
 </div>
