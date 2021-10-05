d0c23386fda65c2e32c6ebce7d990d645fcdd314
Manon Lespes <m.lespes@interface3.be>
2021-10-05 14:24:04 +0200

    travaille sur la class utilisateur

--- utilisation/utilisateur.php
+++ utilisation/utilisateur.php
@@ -1,4 +1,15 @@
 <?php 
-    require_once("../class/Utilisateur.php");
-    $u = new Utilisateur();
+    define('__ROOT__', dirname(dirname(__FILE__)));
+    require_once(__ROOT__."/class/Utilisateur.php");
+    
+
+    /* test qui fonctionne */
+    $Manon = new Utilisateur ("Lespes", "Manon", "Chatshimi", "m.lespes@interface3.be", " ", " ", " ", " ", " ", " ", "001");
+    $Manon->setAdresse("Rue de la victoire");
+    echo "<p>Voici l'adresse de" . $Manon->getPrenom() . " " . $Manon->getAdresse() . "</p>";
+    $Manon->setMdp("azerty");
+    $Manon->verificationConnection();
+    echo "<p>Voici la vérifaction de".$Manon->getPrenom(). " :" . $Manon->verificationConnection()."</p>";
+    
+    
 ?>
