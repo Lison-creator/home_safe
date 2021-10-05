e3ec4ac8fb6c644c1d4773a6f08f8520e8c559df
Lison <lison.ferne@wanadoo.fr>
2021-10-05 16:43:25 +0200

    Commit Lison fin de journée

--- view/page/profil.php
+++ view/page/profil.php
@@ -6,3 +6,31 @@
 <!-- Petit menu vertical: "Nouveautés", "Ma communauté", "Lieux favoris" -->
 <!-- La carte interactive -->
 <!-- Le logo -->
+
+<div class="container">
+
+    <!-- Menu qui tombe en haut à droite-->
+    <nav>
+
+    </nav>
+
+    <!-- logo en haut à gauche -->
+    <img src="" alt="logo">
+
+    <div>
+    </div>
+
+    <!-- Partie à droite -->
+    <aside>
+        <div>
+            <!-- Head: onglet photo de profil + petit onglet toggle pour les options du compte -->
+
+            <!-- Petite flêche vers le bas (svg) -->
+            <svg id="_Color" data-name=" ↳Color" xmlns="http://www.w3.org/2000/svg" width="25.04" height="15.442" viewBox="0 0 25.04 15.442">
+                <path id="_Color-2" data-name=" ↳Color" d="M2.938,0,0,2.942,9.544,12.52,0,22.1,2.938,25.04l12.5-12.52Z" transform="translate(25.04) rotate(90)" fill="#707070" />
+            </svg>
+
+        </div>
+
+    </aside>
+</div>
