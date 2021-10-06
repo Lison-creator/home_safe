e8f1dea64e9b1c62b6c413759ace1283ca3bc660
Maureen Vanhoren <maureenvanhoren@gmail.com>
2021-10-06 12:25:28 +0200

    menu sur le cote de la mort qui tue

--- view/html/footer.php
+++ view/html/footer.php
@@ -2,6 +2,8 @@
 <!-- Peut-être le petit menu déroulant en bas à gauche? il est presque sur toutes les pages -->
 <!-- Demander si c'est possible (comme normalement le footer clot le html)-->
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="public/js/script.js"></script>
 </body>
 </html>
--- view/menu/menu-co.php
+++ view/menu/menu-co.php
@@ -1,7 +1,19 @@
 <!-- Pour l'instant, pas de différence entre le menu connecté et non-connecté... -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav>
+  <ul class="passeDevant">  
+  <li class="collapse blocNav"><div class="navJaune d-flex justify-content-center"><a href="?section=accueil" class="nav-link" id="register">Accueil</a></div></li>
+  <li class="collapse blocNav"><div class="navBleu d-flex justify-content-center"><a href="?section=association" class="nav-link" id="association">A propos</a></div></li>
+  <li class="collapse blocNav"><div class="navViolet d-flex justify-content-center"><a href="?section=contact" class="nav-link" id="contact">Contact</a></div></li>
+  <li class="collapse blocNav"><div class="navJaune d-flex justify-content-center"><a href="?section=faq" class="nav-link" id="faq">FAQ</a></div></li>
+  <li id="iconPlus" class="navBlanc">
+    <div>X</div>
+  </li>
+  </ul>
+</nav>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home Safe</a> <!-- Le index.php ne fonctionne pas, remplacer par un dièse pour l'instant(qui ne marche pas non plus). Voir ce qui est mieux //TODO -->
+    <a class="navbar-brand" href="#">Home Safe</a> Le index.php ne fonctionne pas, remplacer par un dièse pour l'instant(qui ne marche pas non plus). Voir ce qui est mieux //TODO 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,9 +43,9 @@
       </ul>
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a href="?section=deconnexion" class="nav-link" id="deconnexion"><?= $_SESSION['nom']; ?>Se déconnecter</a><!-- rajouter le $session pour appeller la page connexion.php -->
+          <a href="?section=deconnexion" class="nav-link" id="deconnexion"><?= $_SESSION['nom']; ?>Se déconnecter</a> rajouter le $session pour appeller la page connexion.php
         </li>
       </ul>
     </div>
   </div>
-</nav>
+</nav> -->
