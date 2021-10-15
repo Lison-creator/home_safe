413999c9183c14099f1c066946f9ffae93359e4c
Manon Lespes <m.lespes@interface3.be>
2021-10-15 14:07:04 +0200

    js

--- public/js/script.js
+++ public/js/script.js
@@ -17,44 +17,34 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 /* --------------------------Boutons de la page Association au moment du clique */
 /* on veut que l'élément prenne une classe et donc le CSS associé au moment du clic et que cela l'enlève ensuite */
-
 /* Je récupère mon élément avec la classe */
-/* const btnAssociation = document.getElementsByClassName('blocTitresAssociation')[0]; */
-
+const btnAssociation = document.querySelectorAll('.blocTitresAssociation');
 /* ici je crée la fonction toggle avec un callback !!!!*/
-//function toggle(element, classeEnQuestion) {
-    /* si element qui contient la class blocTitresAssociation ne contient pas la class selectionne */
-    //if (!(element.classList.contains(classeEnQuestion))) {
+function toggle(element, classeEnQuestion) {
 
-        /* tu l'as lui rajoute */
-    //    element.classList.add(classeEnQuestion)
-   // }
-    /* autrement tu l'as lui enlève */
-    //else {
-   //     element.classList.remove(classeEnQuestion)
-    //}
-//}
+    btnAssociation.forEach((autreElement) => {
 
+        if (!(autreElement == element)) {
+            element.classList.add(classeEnQuestion)
+        }
+        /* si element qui contient la class blocTitresAssociation ne contient pas en plus la class selectionne */
+        if (!(element.classList.contains(classeEnQuestion))) {
+            /* tu l'as lui rajoute */
+            element.classList.add(classeEnQuestion)
+        }
+        /* autrement tu l'as lui enlève */
+        else {
+            element.classList.remove(classeEnQuestion)
+        }
+    })
+}
 /* ici j'exécute la fonction */
-/* btnAssociation.addEventListener('click', (e) => {
-    toggle(e.target, 'selectionne')
-}) */
-
-$(".blocTitresAssociation").on("click", function(e){
-    $(".blocTitresAssociation").toggleClass("selectionne");
+btnAssociation.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        toggle(e.target, 'selectionne')
+        const xx = btnAssociation.filter(x => !x)
+        console.log(xx);
+    })
 })
-
-/* --------------------------Boutons de la page Association */
