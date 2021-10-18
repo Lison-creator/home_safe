2f3a3cbd13c729020ae6121d624d2d8007e40e5a
Manon Lespes <m.lespes@interface3.be>
2021-10-18 10:55:18 +0200

    Whatever

--- view/html/footer.php
+++ view/html/footer.php
@@ -5,5 +5,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <script src="public/js/script.js"></script>
+<script src="https://kit.fontawesome.com/0e9c9dec41.js" crossorigin="anonymous"></script>
 </body>
 </html>
