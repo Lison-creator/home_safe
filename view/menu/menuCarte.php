b4c4b5425d0aa2f945ff48cffdc82caf44309305
Manon Lespes <manonlespes@hotmail.fr>
2021-10-25 14:19:11 +0200

    petites icones

---
+++ view/menu/menuCarte.php
@@ -0,0 +1,23 @@
+<!-- Menu permanent -->
+<nav class="menuPermanent">
+  <ul class="passeDevant">
+    <li class="collapse-menu blocNav">
+      <div class="navJaune d-flex justify-content-center"><a href="?section=accueil" class="nav-link" id="register">Accueil</a></div>
+    </li>
+    <li class="collapse-menu blocNav">
+      <div class="navBleu d-flex justify-content-center"><a href="?section=association" class="nav-link" id="association">A propos</a></div>
+    </li>
+    <li class="collapse-menu blocNav">
+      <div class="navViolet d-flex justify-content-center"><a href="?section=contact" class="nav-link" id="contact">Contact</a></div>
+    </li>
+    <li class="collapse-menu blocNav">
+      <div class="navJaune d-flex justify-content-center"><a href="?section=faq" class="nav-link" id="faq">FAQ</a></div>
+    </li>
+    <li id="iconPlus" class="navBlanc">
+      <div id="croix" class="animationCroixFun">
+        <div class="vertical"></div>
+        <div class="horizontal"></div>
+      </div>
+    </li>
+  </ul>
+</nav>
