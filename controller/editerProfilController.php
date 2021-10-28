1b108e7fc32e4a6ae48733377ccc89edfe69cfae
Manon Lespes <manonlespes@hotmail.fr>
2021-10-28 08:25:26 +0200

    var dump commenté pour éviter affichage

--- controller/editerProfilController.php
+++ controller/editerProfilController.php
@@ -4,15 +4,15 @@
 include("view/html/head.php");
 
 if (isset($_SESSION)) {
-    var_dump($_SESSION);
+    //var_dump($_SESSION);
     // il/elle est connecté
     include('view/menu/menu-co.php'); 
-    var_dump('je suis dans la boulce du if');
-    var_dump($_SESSION);
+    //var_dump('je suis dans la boulce du if');
+    //var_dump($_SESSION);
     
 } else { // si tu n'es pas connecté
     include('view/menu/menu-nonco.php'); 
-    var_dump('je suis dans la boulce du else');
+    //var_dump('je suis dans la boulce du else');
     
 }
 
